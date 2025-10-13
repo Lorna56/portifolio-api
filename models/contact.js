f884f689dba627148ca72383cf6360ae29819358
Lorna56 <lornanaula0042@gmail.com>
2025-10-13 14:39:28 +0300

    Changes

--- models/contact.js
+++ models/contact.js
@@ -1,15 +1,4 @@
-// // models/Contact.js
-// import mongoose from "mongoose";
 
-// const ContactSchema = new mongoose.Schema({
-//   name: String,
-//   email: String,
-//   message: String,
-//   createdAt: { type: Date, default: Date.now },
-// });
-
-// export default mongoose.model("Contact", ContactSchema);
-// models/Contact.js
 import mongoose from "mongoose";
 
 const ContactSchema = new mongoose.Schema({
